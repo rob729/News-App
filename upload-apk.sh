e68d18509aae07dfe698350b247a830d039ddcb9
Robin <rs729028@gmail.com>
2018-08-24 22:31:07 +0530

    changes

--- upload-apk.sh
+++ upload-apk.sh
@@ -11,7 +11,7 @@
   git config --global user.name "rob729" 
   # Clone the repository in the folder buildApk
 
-  git clone --quiet --branch master = https: // user-name: $GITHUB_API_KEY@github.com/user-name/repo-name master> / dev / null 
+  git clone --quiet --branch master = https: // rob729: $GITHUB_API_KEY@github.com/rob729/News-App master> / dev / null 
   #go into directory and copy data we're interested
 
   cd master cp -Rf $ HOME / android / *. 
@@ -19,7 +19,7 @@
 
   git add -f. 
   git remote rm origin 
-  git remote add origin https: // user-name: $GITHUB_API_KEY@github.com/user-name/repo-name.git
+  git remote add origin https: //rob729: $GITHUB_API_KEY@github.com/rob729/News-App.git
    . git add -f 
   git commit -m "Travis build $ TRAVIS_BUILD_NUMBER pushed [skip ci] " 
   git push origin master -fq> / dev / null 
